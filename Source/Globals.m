55b70fb89e6e6de91d079237a7d8ca7ef2db361f
Jenny <jhl989@mit.edu>
2015-04-07 01:20:55 -0400

    Fixed weird level
    Has cake, requires a bit of cleverness?

--- Source/Globals.m
+++ Source/Globals.m
@@ -21,7 +21,7 @@
 
 - (id)init {
     if (self = [super init]) {
-        NSUInteger highest = 15;
+        NSUInteger highest = 17;
         //NSUInteger highest = [[NSUserDefaults standardUserDefaults] integerForKey:@"highestLevel"];
         if (highest == nil || highest==1) {
             CCLOG(@"new game");
@@ -36,7 +36,7 @@
         audio = [OALSimpleAudio sharedInstance];
         screenSize = [CCDirector sharedDirector].viewSize;
     
-        totalLevels = 15;
+        totalLevels = 17;
     }
     return self;
 }
