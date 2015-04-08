9212ae0005a182dd45b8ece070744b0f5a09ceac
lilisun <lilis@mit.edu>
2015-04-08 16:38:59 -0400

    cutscene goes to the right level

--- Source/Cutscene.m
+++ Source/Cutscene.m
@@ -7,8 +7,10 @@
 //
 
 #import "Cutscene.h"
+#import "Globals.h"
 
 @implementation Cutscene{
+    Globals *globals;
     
     //from spritebuilder
     float levelRotation;
@@ -16,8 +18,16 @@
     BOOL isCutsceneNext;
 }
 
+- (id) init {
+    if (self = [super init]) {
+        globals = [Globals globalManager];
+    }
+    return self;
+}
+
 - (void)next{
     CCLOG(@"cutscene over");
+    [globals setLevel:nextLevel];
     CCScene *gameplayScene = [CCBReader loadAsScene:@"scenes/GamePlay"];
     [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
